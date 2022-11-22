1dbb2ab91c68ce4730b482aacc21ca5727960f5f
Miles Edwards <miles.mfe@gmail.com>
2022-11-22 18:16:03 +0000

    Added remaining CardGameTest tests as per design.

--- src/tests/CardGameTest.java
+++ src/tests/CardGameTest.java
@@ -12,13 +12,40 @@ import static org.junit.Assert.*;
  * 
  */
 public class CardGameTest {
+    CardGame gameTest = new CardGame(4);
+
 
     @Test
     public void testConstructor() {
-        CardGame gameTest = new CardGame(4);
         assertEquals(4, gameTest.getPlayers().size());
         assertEquals(4, gameTest.getDecks().size());
         assertEquals(32, gameTest.getPack().size());
     }
-    
+
+
+    @Test
+    public void testDealHands() {
+        gameTest.dealHands();
+        assertEquals(16, gameTest.getPack().size());
+    }
+
+
+    @Test
+    public void testPopulateDecks() {
+        gameTest.populateDecks();
+        assertEquals(16, gameTest.getPack().size());
+    }
+
+
+    @Test
+    public void testNextTurn() {
+        gameTest.nextTurn();
+        assertEquals(1, gameTest.getPlayerTurn());
+        gameTest.nextTurn();
+        assertEquals(2, gameTest.getPlayerTurn());
+        gameTest.nextTurn();
+        assertEquals(3, gameTest.getPlayerTurn());
+        gameTest.nextTurn();
+        assertEquals(0, gameTest.getPlayerTurn());
+    }
 }
