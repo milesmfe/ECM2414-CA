65fe7016116493711ddbbeeb0aa39091cdd811b0
Miles Edwards <miles.mfe@gmail.com>
2022-11-22 15:17:46 +0000

    Added tests for CardDenominations.

--- src/tests/CardDenominationTest.java
+++ src/tests/CardDenominationTest.java
@@ -1,6 +1,7 @@
 package tests;
 
 import org.junit.Test;
+import cards.CardDenomination;
 import static org.junit.Assert.*;
 
 /**
@@ -13,8 +14,38 @@ import static org.junit.Assert.*;
 public class CardDenominationTest {
 
     @Test
-    public void testSomething() {
-        assertEquals(getClass(), getClass());
+    public void testGetValue() {
+        assertEquals(CardDenomination.ACE.getValue(), 1);
+        assertEquals(CardDenomination.TWO.getValue(), 2);
+        assertEquals(CardDenomination.THREE.getValue(), 3);
+        assertEquals(CardDenomination.FOUR.getValue(), 4);
+        assertEquals(CardDenomination.FIVE.getValue(), 5);
+        assertEquals(CardDenomination.SIX.getValue(), 6);
+        assertEquals(CardDenomination.SEVEN.getValue(), 7);
+        assertEquals(CardDenomination.EIGHT.getValue(), 8);
+        assertEquals(CardDenomination.NINE.getValue(), 9);
+        assertEquals(CardDenomination.TEN.getValue(), 10);
+        assertEquals(CardDenomination.JACK.getValue(), 11);
+        assertEquals(CardDenomination.QUEEN.getValue(), 12);
+        assertEquals(CardDenomination.KING.getValue(), 13);
+    }
+
+
+    @Test
+    public void testValueOf() {
+        assertEquals(CardDenomination.valueOf(1), CardDenomination.ACE);
+        assertEquals(CardDenomination.valueOf(2), CardDenomination.TWO);
+        assertEquals(CardDenomination.valueOf(3), CardDenomination.THREE);
+        assertEquals(CardDenomination.valueOf(4), CardDenomination.FOUR);
+        assertEquals(CardDenomination.valueOf(5), CardDenomination.FIVE);
+        assertEquals(CardDenomination.valueOf(6), CardDenomination.SIX);
+        assertEquals(CardDenomination.valueOf(7), CardDenomination.SEVEN);
+        assertEquals(CardDenomination.valueOf(8), CardDenomination.EIGHT);
+        assertEquals(CardDenomination.valueOf(9), CardDenomination.NINE);
+        assertEquals(CardDenomination.valueOf(10), CardDenomination.TEN);
+        assertEquals(CardDenomination.valueOf(11), CardDenomination.JACK);
+        assertEquals(CardDenomination.valueOf(12), CardDenomination.QUEEN);
+        assertEquals(CardDenomination.valueOf(13), CardDenomination.KING);
     }
     
 }
