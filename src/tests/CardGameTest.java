8469c4818a8677bc0fc143020a501c0caa9bfbb7
Miles Edwards <miles.mfe@gmail.com>
2022-11-23 14:52:25 +0000

    Added tests for deckLeftOf and deckRightOf.

--- src/tests/CardGameTest.java
+++ src/tests/CardGameTest.java
@@ -1,6 +1,9 @@
 package tests;
 
 import org.junit.Test;
+
+import cards.CardDeck;
+import cards.CardGame;
 import static org.junit.Assert.*;
 
 /**
@@ -14,7 +17,19 @@ public class CardGameTest {
 
 
     @Test
-    public void testSomething() {
-        assertEquals(getClass(), getClass());
+    public void testdeckLeftRightOf() {
+        CardGame testGame = new CardGame(4);
+        CardDeck left; CardDeck right;
+        testGame.setupGame();
+
+        left = testGame.deckLeftOf(1);
+        right = testGame.deckRightOf(1);
+        assertEquals(1, left.getDeckNumber());
+        assertEquals(2, right.getDeckNumber());
+
+        left = testGame.deckLeftOf(4);
+        right = testGame.deckRightOf(4);
+        assertEquals(4, left.getDeckNumber());
+        assertEquals(1, right.getDeckNumber());
     }
 }
