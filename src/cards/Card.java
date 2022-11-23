81dffd185d49b21c51844ca63dc8aedbb76a0be8
Sophie Chen <sophie.suchen@outlook.com>
2022-11-23 11:40:26 +0000

    update card class

--- src/cards/Card.java
+++ src/cards/Card.java
@@ -25,6 +25,9 @@ public class Card {
          * is implemented. Replaced by public Card(CardDenomination d) {}
          * 
          */
+        Random rand = new Random();
+        int randomNum = rand.nextInt((13 - 1) + 1) + 1;
+        denomination = CardDenomination.valueOf(randomNum);
     }
 
 
